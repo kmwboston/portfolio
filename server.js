b7a022b7ef4d540e795b2ba5ec47aa4d3140641d
Kyle <Tigjer@Kyles-MacBook-Pro.local>
2018-06-25 20:36:16 -0400

    added build processes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -33,5 +34,13 @@ app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
